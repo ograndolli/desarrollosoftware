a9c56c419527381b9c70195414447d95627a004f
Oriana Grandolli <ograndolli@facultad.sanfrancisco.utn.edu.ar>
2023-08-14 21:04:23 -0300

    Eliminar

--- Ejercicio0.py
+++
@@ -1,7 +0,0 @@
-n1=int(input("\nIngrese el primer numero: "))
-n2=int(input("Ingrese el segundo numero: "))
-n3=int(input("Ingrese el tercer numero: "))
-
-resultado=n1+n2+n3
-
-print(f"\nLa suma de los numeros ingresados es: {resultado}")
